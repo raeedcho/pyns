d1d0aaea5936b982532c3cd5b5eb401913692397
elliottb <elliottb@fc757560-3fd6-384f-a8de-a9314be24fd0>
2012-10-11 18:06:22 +0000

    Added license field to the setup classifiers.
    git-svn-id: https://192.168.139.241/svn/SW/tools/python/pyns/trunk@707 fc757560-3fd6-384f-a8de-a9314be24fd0

--- setup.py
+++ setup.py
@@ -6,13 +6,14 @@ classifiers =[
               'Operating System :: OS Independent',
               'Programming Language :: Python',
               'Topic :: Scientific/Engineering',
-              'Topic :: Software Development :: Libraries :: Python Modules'
+              'Topic :: Software Development :: Libraries :: Python Modules',
+              'License :: OSI Approved :: GNU General Public License (GPL)'
               ]
 
 setup(name="Py Neuroshare",
-      version="0.4",
+      version="0.4.2",
       description="Python port of the Neuroshare API",
-      author="Ripple LLC.",
+      author="Ripple LLC",
       author_email="support@rppl.com",
       packages=["pyns"],
       classifiers=classifiers
